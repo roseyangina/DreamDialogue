cb0a7f6fa8560d706e3d90ecd94849377be115e7
jontrn <tranjonathan0917@gmail.com>
2024-10-27 00:23:07 -0400

    Web config file

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js');
 const app = express()
+const cors = require('cors');
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 
 // routes
@@ -15,11 +17,11 @@ app.use('/api/products', productRoute);
 mongoose.connect("mongodb+srv://tranjonathan0917:DreamDialogue@dreamdialogue.bbacb.mongodb.net/?retryWrites=true&w=majority&appName=DreamDialogue")
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 3000}`);
         });
     })
     .catch(() => {
-        console.olog("Connected failed!");
+        console.log("Connected failed!");
     });
     
