f261ef6bf231303c8ddfd0c259073477b821fab7
RatthewThePlagueBringer <matthewgrimaldi111@gmail.com>
2024-10-28 17:35:24 -0400

    Revert "Merge pull request #6 from RatthewThePlagueBringer/jon-branch"
    This reverts commit 30b59c96bf07ea9702bfc7615924ccffb3058d92, reversing
    changes made to 82c9e1893dbb19be7c28b5d5164d39e25911fc06.

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js');
 const app = express()
-const cors = require('cors');
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
 
 // routes
@@ -17,11 +15,11 @@ app.use('/api/products', productRoute);
 mongoose.connect("mongodb+srv://tranjonathan0917:DreamDialogue@dreamdialogue.bbacb.mongodb.net/?retryWrites=true&w=majority&appName=DreamDialogue")
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 3000}`);
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
         });
     })
     .catch(() => {
-        console.log("Connected failed!");
+        console.olog("Connected failed!");
     });
     
